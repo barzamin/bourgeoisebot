f71874945b06ac24d2283a89a6fd48373668a2f1
Erin <sylphofelectricity@gmail.com>
2016-12-29 05:43:26 +0000

    Finish help plugin

--- plugins/help.js
+++ plugins/help.js
@@ -1,12 +1,21 @@
 const debug = require('debug')('plugin-help');
 const {command} = require('./common');
+const _ = require('lodash');
 
 const Help = {
   name: "Help",
+  docs: {
+    'help': 'Get bot help',
+  },
   
   actions: [
     [command('!help'), function (message, bot) {
-      message.reply(`⟹ cuddlebot v1.0-alpha ⟸`);
+      message.reply(`⟹ cuddlebot v1.0-alpha ⟸\n***plugins***:\n`
+      + this.plugins.map(p => {
+        return `⟿ **${p.name}**\n`
+          + _.map(p.docs, (d, c) => `   » **${this.config.prefix}${c}** ➩ ${d}`);
+        }).join('\n')
+      );
     }]
   ]
 };
